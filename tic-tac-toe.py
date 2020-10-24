7a6dbf011182041229df54ebcbdf6b787ccc6921
ugauniyal <ugauniyal@outlook.com>
2020-10-24 20:10:28 +0530

    introduce game class

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,142 +3,131 @@ import random
 import os
 from terminaltables import AsciiTable, SingleTable
 
-board = ['_', '_', '_',
-         '_', '_', '_',
-         '_', '_', '_']
 
-continue_game = 'Y'
+class TicTacToe:
 
-player = 'X'
+    def reset(self):
+        self.board = ['_', '_', '_',
+                      '_', '_', '_',
+                      '_', '_', '_']
+        self.set_game_state()
+        self.player = 'O'
+        self.winner = None
+        self.player_names = []
 
+    def set_game_state(self, start=True):
+        self.game_in_progress = start
 
-winner = None
-game_in_progress = True
+    def __init__(self):
+        self.reset()
 
+    def set_player_names(self, player1, player2):
+        self.player_names = [player1, player2]
+        random.shuffle(self.player_names)
 
-# Board of Game
-def show_board():
-    data = []
-    data.append([board[0], board[1], board[2]])
-    data.append([board[3], board[4], board[5]])
-    data.append([board[6], board[7], board[8]])
-    table_result = SingleTable(data)
-    table_result.inner_row_border = True
-    print(table_result.table)
-    print('\n')
+    def get_player_names(self):
+        return self.player_names
 
-# Handling the first turn
+    def get_current_player_name(self):
+        if self.player == 'X':
+            return self.player_names[0]
+        else:
+            return self.player_names[1]
 
+    def update_board(self, pos):
+        valid_inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        if pos not in valid_inputs:
+            return False
+        self.board[pos-1] = self.player
+        return True
 
-def first_turn():
-    print('\n')
-    player1 = input('Enter your name ')
-    player2 = input('Enter your name ')
-    players = [player1, player2]
-    print('\n')
-    print(random.choice(players) + ' will play first.')
-    print('\n')
+    def get_player(self):
+        return self.player
 
-# Play Game
+    def swap_player(self):
+        if self.player == 'X':
+            self.player = 'O'
+        else:
+            self.player = 'X'
 
+    def check_win(self):
+        win_situation = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [
+            0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-def play_game():
-    valid_inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    global continue_game
-    global board
-    global game_in_progress
-    while game_in_progress and win_check(player) == None and tie_check() == None:
-        position_input = int(
-            input('Enter the position of your choice (1-9): '))
+        for wins in win_situation:
+            if all(self.board[pos] == self.player for pos in wins):
+                self.set_game_state(False)
+                self.winner = self.get_player()
+                return True
+
+        return False
+
+    def get_winner(self):
+        return self.winner
+
+    def check_tie(self):
+        if "_" not in self.board:
+            return True
+        else:
+            return False
+
+    def is_running(self):
+        return self.game_in_progress and self.check_win() == False and self.check_tie() == False
+
+    def show_board(self):
+        data = []
+        data.append([self.board[0], self.board[1], self.board[2]])
+        data.append([self.board[3], self.board[4], self.board[5]])
+        data.append([self.board[6], self.board[7], self.board[8]])
+        table_result = SingleTable(data)
+        table_result.inner_row_border = True
+        print(table_result.table)
         print('\n')
 
-        if position_input not in valid_inputs:
-            print('The position is invalid. Please enter between 1 and 9')
-            continue
 
-        pos = position_input - 1
-        if board[pos] != '_':
-            print('The position is already filled, choose other position')
+def play_game(game):
+    game.reset()
+    print('\n')
+    player1 = input('Enter your name: ')
+    player2 = input('Enter your name: ')
+    print('\n')
+    game.set_player_names(player1, player2)
+
+    print(
+        f'{game.get_player_names()[0]} will play X and {game.get_player_names()[1]} will play O')
+    print('\n')
+
+    while game.is_running():
+        game.show_board()
+        game.swap_player()
+        position_input = int(input(
+            f'Enter the position {game.get_current_player_name()}: '))
+        print('\n')
+        if not game.update_board(position_input):
+            print('Wrong position entered')
             print('\n')
             continue
 
-        board[pos] = player
-        show_board()
-        win_check(player)
-        print('\n')
-        turn_swap()
-
-
-# Swap the turns between the players
-def turn_swap():
-    global player
-    player = 'X' if player != 'X' else 'O'
-
-# Check if someone won
-
-
-def win_check(player):
-    global board
-    global winner
-    global game_in_progress
-
-    win_situation = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [
-        0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
-    for wins in win_situation:
-        if all(board[pos] == player for pos in wins):
-            game_in_progress = False
-            winner = player
-            print(winner, 'is the winner')
-            return True
-
-    return None
-
-
-# Check if the game is tied
-def tie_check():
-    global game_in_progress
-    global continue_game
-    if "_" not in board:
+    winner = game.get_winner()
+    if winner == None:
+        game.show_board()
         print('Game Tie')
-        return True
-    else:
-        return None
+        print('\n')
+    elif winner == 'X':
+        game.show_board()
+        print(game.get_player_names()[0], 'is the winner')
+        print('\n')
+    elif winner == 'O':
+        game.show_board()
+        print(game.get_player_names()[1], 'is the winner')
+        print('\n')
+    
 
-# Asking if Player wants to play again
-
-
-def ask_continue_game():
-    global board
-    global continue_game
-    global game_in_progress
-    global player
-
+game = TicTacToe()
+choice = 'Y'
+while choice == 'Y' or choice == 'y':
+    columns = shutil.get_terminal_size().columns
+    print("TIC-TAC-TOE".center(columns))
+    play_game(game)
+    choice = input('Do you want to play again? (Y/N)')
     print('\n')
-    print('Do you want to play again? To play again type "Y" or press any other key to suspend.')
-    continue_game = input()
-    print('\n')
-
-    while continue_game == 'Y':
-
-        game_in_progress = True
-        board = ['_', '_', '_',
-                 '_', '_', '_',
-                 '_', '_', '_']
-        player = 'X'
-        play_game()
-        ask_continue_game()
-
-    else:
-        return False
-
-
-columns = shutil.get_terminal_size().columns
-print("TIC-TAC-TOE".center(columns))
-
-
-print('\n')
-show_board()
-first_turn()
-play_game()
-ask_continue_game()
